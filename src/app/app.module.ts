94853c391c3d3644a95a7ed803dfe440c0f7936b
Sam <sam.j1912@hotmail.com>
2020-03-21 09:51:20 -0400

    Add Amexio on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {AmexioWidgetModule} from 'amexio-ng-extensions'; // Import Amexio library
+//Dashboard,Charts & Maps are available as seperate module (not in AmexioWidgetModule)
+import {AmexioChartsModule,AmexioDashBoardModule,AmexioEnterpriseModule,AmexioMapModule} from 'amexio-ng-extensions';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, AmexioWidgetModule, FormsModule, AmexioChartsModule, AmexioDashBoardModule,AmexioEnterpriseModule,AmexioMapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
